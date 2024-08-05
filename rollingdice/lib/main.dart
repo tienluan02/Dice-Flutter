e370a86193ea4c89e9fbdd2da18252ab30596d65
tienluandev <nguyentienluan3@gmail.com>
2024-08-05 15:53:26 +0700

    finish the Dice project

--- rollingdice/lib/main.dart
+++ rollingdice/lib/main.dart
@@ -1,63 +1,72 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.black,
         appBar: AppBar(
-          backgroundColor: Colors.blue,
           title: const Center(
             child: Text(
               'Dice',
               style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.bold,
-                fontSize: 20.0,
+                fontSize: 30.0,
               ),
             ),
           ),
+          backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: MyHomePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatefulWidget {
-  const DicePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
   @override
-  State<DicePage> createState() => _DicePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 2;
-  int rightDiceNumber = 4;
+class _MyHomePageState extends State<MyHomePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceNumber() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
-          children: [
-            Expanded(
-              child: TextButton(
-                  onPressed: () {
-                    print('left button is pressed');
-                  },
-                  child: Image.asset('images/dice$leftDiceNumber.png')
-              ),
-            ),
-            Expanded(
-              child: TextButton(
-                  onPressed: () {
-                    print('right button is pressed');
-                  },
-                  child: Image.asset('images/dice$rightDiceNumber.png')),
-            ),
-          ]
-      ),
+      child: Row(children: [
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              changeDiceNumber();
+            },
+            child: Image.asset('images/dice$leftDiceNumber.png'),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                changeDiceNumber();
+              });
+            },
+            child: Image.asset('images/dice$rightDiceNumber.png'),
+          ),
+        ),
+      ]),
     );
   }
 }
